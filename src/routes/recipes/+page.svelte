a62a83ba65a938b4f04c04cee88e5115922501aa
Kyle Horton <kylehorton33@gmail.com>
2023-12-13 14:57:05 -0500

    feat: recipe cards

--- src/routes/recipes/+page.svelte
+++ src/routes/recipes/+page.svelte
@@ -4,10 +4,15 @@
 
 <h1 class="text-center pt-6 pb-10">Recipes</h1>
 
-<ul class="grid mx-auto max-w-5xl gap-4 justify-center">
+<ul class="grid md:grid-cols-2 2xl:grid-cols-3 mx-auto gap-4 justify-center">
 	{#each data.recipes as { name, slug }}
-		<li>
-			<a href="/recipes/{slug}">{name}</a>
+		<li class="mx-auto">
+			<a href="/recipes/{slug}">
+				<div class="block card card-hover p-2 w-72">
+					<header class="card-header uppercase">{name}</header>
+					<section class="p-4">(content)</section>
+				</div>
+			</a>
 		</li>
 	{/each}
 </ul>
