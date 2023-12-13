ecea4a017fde0914ab1042bc49eeb896ba4b99fe
Kyle Horton <kylehorton33@gmail.com>
2023-12-13 13:32:34 -0500

    refactor: :recycle: dynamically update links

--- src/lib/components/FooterLink.svelte
+++ src/lib/components/FooterLink.svelte
@@ -1,21 +1,11 @@
 <script lang="ts">
 	import { TabAnchor } from '@skeletonlabs/skeleton';
+	//@ts-ignore
+	import { page } from '$app/stores';
 	export let route: string;
 </script>
 
-{#if route == 'ingredients'}
-	<TabAnchor selected href="/ingredients">
-		<svelte:fragment slot="lead">(icon)</svelte:fragment>
-		<span>Ingredients</span>
-	</TabAnchor>
-{:else if route == 'recipes'}
-	<TabAnchor href="/recipes">
-		<svelte:fragment slot="lead">(icon)</svelte:fragment>
-		<span>Recipes</span>
-	</TabAnchor>
-{:else if route == 'about'}
-	<TabAnchor href="/about">
-		<svelte:fragment slot="lead">(icon)</svelte:fragment>
-		<span>About</span>
-	</TabAnchor>
-{/if}
+<TabAnchor selected={$page.url.pathname == `/${route}`} href="/{route}" title={route}>
+	<svelte:fragment slot="lead">(icon)</svelte:fragment>
+	<span class="capitalize">{route}</span>
+</TabAnchor>
--- src/lib/components/SidebarLink.svelte
+++ src/lib/components/SidebarLink.svelte
@@ -1,21 +1,11 @@
 <script lang="ts">
 	import { AppRailAnchor } from '@skeletonlabs/skeleton';
+	//@ts-ignore
+	import { page } from '$app/stores';
 	export let route: string;
 </script>
 
-{#if route == 'ingredients'}
-	<AppRailAnchor selected href="/ingredients" title="Ingredients">
-		<svelte:fragment slot="lead">(icon)</svelte:fragment>
-		<span>Ingredients</span>
-	</AppRailAnchor>
-{:else if route == 'recipes'}
-	<AppRailAnchor href="/recipes" title="Recipes">
-		<svelte:fragment slot="lead">(icon)</svelte:fragment>
-		<span>Recipes</span>
-	</AppRailAnchor>
-{:else if route == 'about'}
-	<AppRailAnchor href="/about" title="About">
-		<svelte:fragment slot="lead">(icon)</svelte:fragment>
-		<span>About</span>
-	</AppRailAnchor>
-{/if}
+<AppRailAnchor selected={$page.url.pathname == `/${route}`} href="/{route}" title={route}>
+	<svelte:fragment slot="lead">(icon)</svelte:fragment>
+	<span class="capitalize">{route}</span>
+</AppRailAnchor>
