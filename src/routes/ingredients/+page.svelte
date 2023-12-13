56fe2a97ee78356ee5a0089c34e6382737732267
Kyle Horton <kylehorton33@gmail.com>
2023-12-13 16:44:49 -0500

    search bars

--- src/routes/ingredients/+page.svelte
+++ src/routes/ingredients/+page.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
 	export let data;
+	let filterTerm: string = "";
 </script>
 
-<h1 class="text-center pt-6 pb-10">Ingredients</h1>
-
-<ul class="grid mx-auto max-w-5xl md:grid-cols-3 xl:grid-cols-4 gap-4 justify-center">
-	{#each data.ingredients as ingredient}
-		<li>
-			<label for={ingredient.name} class="flex items-center space-x-2">
-				<input class="checkbox" type="checkbox" checked={ingredient.inStock} />
-				<span>{ingredient.name}</span>
-			</label>
-		</li>
-	{/each}
-</ul>
+<div class="grid gap-4">
+	<h1 class="text-center pt-6">Ingredients</h1>
+	<div class="w-72 mx-auto input-group input-group-divider grid-cols-[auto_1fr_auto]">
+		<div class="input-group-shim">(i)</div>
+		<input type="search" placeholder="Search..." bind:value={filterTerm} />
+	</div>
+	<ul class="grid mx-auto max-w-5xl md:grid-cols-3 xl:grid-cols-4 gap-4 justify-center">
+		{#each data.ingredients as ingredient}
+			<li class="{ingredient.name.toLocaleLowerCase().includes(filterTerm.toLowerCase()) ? "" : "hidden"} w-48">
+				<label for={ingredient.name} class="flex items-center space-x-2">
+					<input class="checkbox" type="checkbox" checked={ingredient.inStock} />
+					<span>{ingredient.name}</span>
+				</label>
+			</li>
+		{/each}
+	</ul>
+</div>
--- src/routes/recipes/+page.svelte
+++ src/routes/recipes/+page.svelte
@@ -1,19 +1,31 @@
 <script lang="ts">
 	export let data;
+	let filterTerm: string = '';
 </script>
 
-<h1 class="text-center pt-6 pb-10">Recipes</h1>
-
-<ul class="grid md:grid-cols-2 2xl:grid-cols-3 mx-auto gap-4">
-	{#each data.recipes as { name, slug, ingredientLines, missing }}
-		<li>
-			<a href="/recipes/{slug}">
-				<div class="block card card-hover p-2 w-72 h-32 mx-auto relative rounded-lg">
-					<span class="badge {missing ? "variant-filled-warning" : "variant-filled-success"} absolute top-3 right-3">{missing}</span>
-					<header class="card-header uppercase">{name}</header>
-					<section class="p-4 text-xs">{ingredientLines?.map((i) => i.ingredient).join(' | ')}</section>
-				</div>
-			</a>
-		</li>
-	{/each}
-</ul>
+<div class="grid gap-4">
+	<h1 class="text-center pt-6">Recipes</h1>
+	<div class="w-72 mx-auto input-group input-group-divider grid-cols-[auto_1fr_auto]">
+		<div class="input-group-shim">(i)</div>
+		<input type="search" placeholder="Search..." bind:value={filterTerm} />
+	</div>
+	<ul class="grid md:grid-cols-2 2xl:grid-cols-3 mx-auto gap-4">
+		{#each data.recipes as { name, slug, ingredientLines, missing }}
+			<li class="{name.toLocaleLowerCase().includes(filterTerm.toLowerCase()) ? "" : "hidden"}">
+				<a href="/recipes/{slug}">
+					<div class="block card card-hover p-2 w-72 h-32 mx-auto relative rounded-lg">
+						<span
+							class="badge {missing
+								? 'variant-filled-warning'
+								: 'variant-filled-success'} absolute top-3 right-3">{missing}</span
+						>
+						<header class="card-header uppercase">{name}</header>
+						<section class="p-4 text-xs">
+							{ingredientLines?.map((i) => i.ingredient).join(' | ')}
+						</section>
+					</div>
+				</a>
+			</li>
+		{/each}
+	</ul>
+</div>
