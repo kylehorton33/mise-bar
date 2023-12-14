1bfcc93f558001c5e83e5e2f6146f980c386108d
Kyle Horton <kylehorton33@gmail.com>
2023-12-14 08:27:11 -0500

    feat: :construction: WIP, combine multiple queries for recipe, ingredients, lines

--- src/routes/recipes/+page.server.ts
+++ src/routes/recipes/+page.server.ts
@@ -1,7 +1,18 @@
 import type { Actions, PageServerLoad } from './$types';
 import { recipes, ingredientLines } from '$lib/data';
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async ({locals}) => {
+	try {
+		const allRecipes = await locals.pb.collection('recipes').getFullList({sort: 'name', fields: 'id, name' });
+		const allIngredients = await locals.pb.collection('ingredients').getFullList({sort: 'name', fields: 'id, name, inStock' });
+		const allRecipeLines = await locals.pb.collection('recipe_lines').getFullList({sort: 'recipe', fields: 'recipe, ingredient, quantity' });
+
+		// figure out how to fold all this data together
+		
+	} catch (error) {
+		console.log("[PB:] ", error)
+	}
+
 	// mock API call to get list of all recipes
 	const recipeList: { recipe: Recipe; ingredients: IngredientLine[]; missing: number }[] =
 		[];
