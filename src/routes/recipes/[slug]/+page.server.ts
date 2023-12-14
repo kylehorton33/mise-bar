b33b0395879cf6ac5d16867cd4732deefcc824d8
Kyle Horton <kylehorton33@gmail.com>
2023-12-14 12:26:45 -0500

    feat: :sparkles: single page pulled form pocketbase

--- src/routes/recipes/[slug]/+page.server.ts
+++ src/routes/recipes/[slug]/+page.server.ts
@@ -2,7 +2,32 @@ import type { PageServerLoad } from './$types';
 import { recipes, ingredientLines } from '$lib/data';
 import { error } from '@sveltejs/kit';
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ params, locals }) => {
+	try {
+		const recipe = await locals.pb.collection('recipes').getFirstListItem(`slug="${params.slug}"`);
+		const ingredientLines = await locals.pb.collection('ingredientLines').getFullList({ filter: `recipe="${recipe.id}"` });
+		const getIngredients = async () => {
+			return Promise.all(ingredientLines.map(async (l) => {
+				const ingredient = await locals.pb.collection('ingredients').getOne(l.ingredient);
+				return {
+					quantity: l.quantity,
+					ingredient: {
+						name: ingredient.name,
+						inStock: ingredient.inStock,
+					}
+				}
+			}))
+		}
+		recipe.ingredients = await getIngredients()
+		//console.log(ingredients)
+
+
+		console.log(recipe)
+		return { recipe };
+	} catch (error) {
+		console.log(error.response.code, error.response.message)
+	}
+
 	const recipe = recipes.find((r) => r.slug == params.slug);
 
 	if (!recipe) {
--- src/routes/recipes/[slug]/+page.svelte
+++ src/routes/recipes/[slug]/+page.svelte
@@ -4,11 +4,11 @@
 </script>
 
 <div class="grid gap-4 mt-12 justify-center">
-	<h1 class="text-xl text-center">{data.fullRecipe.recipe.name}</h1>
+	<h1 class="text-xl text-center">{data.recipe.name}</h1>
 	<hr />
 	<ul class="list-disc list-inside w-72">
-		{#each data.fullRecipe.ingredients as { ingredient, quantity }}
-			<li>{quantity} oz {ingredient.name}</li>
+		{#each data.recipe.ingredients as { ingredient, quantity }}
+			<li class="{ingredient.inStock ? "" : "text-error-800"}">{quantity} oz {ingredient.name}</li>
 		{/each}
 	</ul>
 	<p class="w-72">
@@ -16,5 +16,5 @@
 		harum voluptas quo, blanditiis repellendus deserunt, libero nam aspernatur? Provident quo quis
 		suscipit repudiandae nemo neque.
 	</p>
-	<img src={img} class="h-48 mx-auto rounded-lg" alt={data.fullRecipe.recipe.name} />
+	<img src={img} class="h-48 mx-auto rounded-lg" alt={data.recipe.name} />
 </div>
