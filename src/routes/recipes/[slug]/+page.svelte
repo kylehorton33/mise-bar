db3056bb5aef3d654b53b69667bff0c5259a25f0
Kyle Horton <kylehorton33@gmail.com>
2023-12-13 19:31:39 -0500

    styling the single recipe page, static image

--- src/routes/recipes/[slug]/+page.svelte
+++ src/routes/recipes/[slug]/+page.svelte
@@ -1,10 +1,20 @@
 <script>
 	export let data;
+	import img from '$lib/images/naked-and-famous.jpg';
 </script>
 
-<h1>{data.fullRecipe.recipe.name}</h1>
-<ul>
-	{#each data.fullRecipe.ingredients as { ingredient, quantity }}
-		<li>{quantity} oz {ingredient.name}</li>
-	{/each}
-</ul>
+<div class="grid gap-4 mt-12 justify-center">
+	<h1 class="text-xl text-center">{data.fullRecipe.recipe.name}</h1>
+	<hr />
+	<ul class="list-disc list-inside w-72">
+		{#each data.fullRecipe.ingredients as { ingredient, quantity }}
+			<li>{quantity} oz {ingredient.name}</li>
+		{/each}
+	</ul>
+	<p class="w-72">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, eos? Unde illum aliquam, dolores
+		harum voluptas quo, blanditiis repellendus deserunt, libero nam aspernatur? Provident quo quis
+		suscipit repudiandae nemo neque.
+	</p>
+	<img src={img} class="h-48 mx-auto rounded-lg" alt={data.fullRecipe.recipe.name} />
+</div>
