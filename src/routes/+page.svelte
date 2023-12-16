9c12b9edb6e51b186c4a58b713ea065107945f89
Kyle Horton <kylehorton33@gmail.com>
2023-12-16 10:01:10 -0500

    fix: :bug: fix some style bugs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,17 @@
 <div class="mt-8 grid gap-4 justify-center">
 	<h1 class="h1 text-center">How to use:</h1>
 	<hr class="my-2" />
-	<ol class="list-decimal grid gap-8 text-center">
+	<ol class="list-decimal grid gap-8 px-12">
 		<li class="text-xl">
 			Mark which <a href="/ingredients" class="code">ingredients</a> you have
 		</li>
 		<li class="justify-center flex gap-12">
 			<div>
-				<input class="checkbox disabled:opacity-25" type="checkbox" checked={false} />
+				<input class="checkbox" disabled type="checkbox" />
 				<span>Out of Stock</span>
 			</div>
 			<div>
-				<input class="checkbox disabled:opacity-25" type="checkbox" checked={true} />
+				<input class="checkbox" disabled type="checkbox" checked />
 				<span>In Stock</span>
 			</div>
 		</li>
