db933ba615166bd3fb9ffc61126246a09fc823a0
Kyle Horton <kylehorton33@gmail.com>
2023-12-16 09:41:27 -0500

    feat: :rocket: v0.1.0

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Plans
 
-# Bold new idea 2023.12.15
+## ~~Bold new idea 2023.12.15~~
 
-- revise most of app to be static and only track updates to ingredient stock through localStorage/stores
+- [DONE!] revise most of app to be static and only track updates to ingredient stock through localStorage/stores
+- [DONE!] deploy v0.1.0 to vercel
+
+## Next steps? 12.16
+
+- add more ing and recipes
+- user tests
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,33 @@
-TODO index page
+<div class="mt-8 grid gap-4 justify-center">
+	<h1 class="h1 text-center">Welcome to <code class="text-3xl code">mise</code></h1>
+	<hr class="my-2" />
+	<ol class="list-decimal grid gap-8 text-center">
+		<li class="text-xl">
+			Mark which <a href="/ingredients" class="code">ingredients</a> you have
+		</li>
+		<li class="justify-center flex gap-12">
+			<div>
+				<input class="checkbox disabled:opacity-25" type="checkbox" checked={false} />
+				<span>Out of Stock</span>
+			</div>
+			<div>
+				<input class="checkbox disabled:opacity-25" type="checkbox" checked={true} />
+				<span>In Stock</span>
+			</div>
+		</li>
+		<li class="text-xl">
+			Search <a href="/recipes" class="code">recipes</a> to see what you can make
+		</li>
+		<li class="justify-center flex gap-12">
+			<div>
+				<span class="badge variant-soft-warning">2</span>
+				<span>Missing 2</span>
+			</div>
+			<div>
+				<span class="badge variant-filled-success">0</span>
+				<span>Ready to make</span>
+			</div>
+		</li>
+		<li class="text-xl">Enjoy!</li>
+	</ol>
+</div>
