ec4b1efdfa29a4c987cfb5ce74ed256c3cf858db
Kyle Horton <kylehorton33@gmail.com>
2023-12-16 21:43:57 -0500

    fix TS error

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,14 +1,14 @@
-import type { PageServerLoad } from './$types';
+import type { LayoutServerLoad } from './$types';
 import { error } from '@sveltejs/kit';
 
-export const load: PageServerLoad = async ({ locals }) => {
+export const load: LayoutServerLoad = async ({ locals }) => {
 	try {
 		const ingredients = await locals.pb.collection('ingredients')
-								.getFullList({ sort: '-updated', fields: 'name' });
+			.getFullList({ sort: '-updated', fields: 'name' });
 		const recipes = await locals.pb.collection('recipes')
-		.getFullList({ sort: '-updated', fields: 'name' });
+			.getFullList({ sort: '-updated', fields: 'name' });
 		const ingredientLines = await locals.pb.collection('ingredientLines')
-		.getFullList({ sort: '-updated', fields: 'recipe, ingredient, quantity' });
+			.getFullList({ sort: '-updated', fields: 'recipe, ingredient, quantity' });
 		return { ingredients, recipes, ingredientLines }
 
 	} catch (e) {
